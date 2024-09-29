d96c3762b0b9d0253beecf27d760c98989562a75
Bharavi Lakhote <bharavi15@gmail.com>
2024-09-30 01:42:56 +0530

    Added decoding logic

--- main.py
+++ main.py
@@ -25,39 +25,74 @@ def string_to_binary_array(s):
     binary_values = [format(ord(char), "08b") for char in s]
     # Create a NumPy array from the binary values
     log.info("binval %s", binary_values)
-    binary_array = np.array([int(bit) for bit in "".join(binary_values)])
+    binary_array = np.array(
+        [int(bit) for bit in "".join(binary_values)], dtype=np.uint8
+    )
     return binary_array
 
 
-def encode_data_to_image(image, data):
+def encode_data_to_image(image, string_to_hide):
+    if len(string_to_hide) > image.shape[0] // 8:
+        raise ValueError("String to long to encode in the given image")
+    bstring = string_to_binary_array(string_to_hide)
+    length_in_binary = "{0:016b}".format(len(bstring))
+    log.info("Length of bstring= %s", len(bstring))
+    data_to_encode = np.append(
+        np.array(list(length_in_binary), dtype=np.uint8), bstring
+    )
     output_image = image.copy()
-    for i in range(len(data)):
-        output_image[i] = encode_value(image[i], data[i])
+    for i in range(len(data_to_encode)):
+        output_image[i] = encode_value(image[i], data_to_encode[i])
     return output_image
 
 
-def main(input_image):
+def decode_data_from_image(input_image) -> str:
+    length_of_data_bin_arr = input_image[:16]
+    length_of_data_bin = np.bitwise_and(length_of_data_bin_arr, 1)
+    op_length_str = ""
+    for i in length_of_data_bin:
+        op_length_str += str(i)
+    output_data_length = int(op_length_str, 2)
+    log.info(f"Length of data = {output_data_length}")
+    data = input_image[16 : 16 + output_data_length]
+    # data = input_image[17 : 17 + (output_data_length * 8)]
+    data = np.bitwise_and(data, 1)
+    print(data)
+    data_str = ""
+    for i in range(0, len(data), 8):
+        op_length_str = ""
+        for j in data[i : i + 8]:
+            op_length_str += str(j)
+        data_str += chr(int(op_length_str, 2))
+    log.info("Data found: " + data_str)
+    return data
+
+
+def main_encode_image(input_image):
     img_shape = input_image.shape
     log.info(f"Input image size= {img_shape}")
     flat_image = convert_img_to_1d_arr(input_image)
     log.info(f"flat_image image size= {flat_image.shape}")
-    log.info(f"You can hide at most {flat_image.shape[0]//8} characters")
+
+    log.info(f"You can hide at most {input_image.shape[0]//8} characters")
     string_to_hide = input("Enter the data to be hidden: ")
 
-    bstring = string_to_binary_array(string_to_hide)
-    log.info("Length of bstring= %s", len(bstring))
-    flat_output_image = encode_data_to_image(flat_image, bstring)
+    flat_output_image = encode_data_to_image(flat_image, string_to_hide)
+
     output_image = flat_output_image.reshape(img_shape)
     imageIO.write_image("encoded_image.png", output_image)
-    # # log.info(flat_image.reshape(img_shape))
-    # output_image = np.bitwise_xor(flat_image,1)
-    # decrypted_image = np.bitwise_xor(output_image,1)
-    # # for i in range(flat_image):
-    # #     output_image = flat_image[i] & 1
-    # log.info(output_image.reshape(img_shape))
-    # log.info(decrypted_image.reshape(img_shape))
+
+
+def main_decode_image(input_image):
+    img_shape = input_image.shape
+    log.info(f"Input image size= {img_shape}")
+    flat_image = convert_img_to_1d_arr(input_image)
+    log.info(f"flat_image image size= {flat_image.shape}")
+    decode_data_from_image(flat_image)
 
 
 if __name__ == "__main__":
-    input_image = imageIO.read_image("blank_black.png")
-    main(input_image)
+    # input_image = imageIO.read_image("DSCN0982.png")
+    # main_encode_image(input_image)
+    input_image = imageIO.read_image("encoded_image.png")
+    main_decode_image(input_image)
